8bccfce93c5c7ab305c086d580c1429714230293
filemort <filemort@gmail.com>
2025-03-08 20:11:39 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+
+$contra_entrar="1234";
+$usu_entrar="Juan";
+
+$el_usuario=isset($_GET['usuario']) ? $_GET['usuario'] : $POST['usuario'];
+
+$la_contra=isset($_GET['contra']) ? $_GET['contra'] : $POST['contra'];
+
+if ($el_usuario == $usu_entrar && $la_contra == $contra_entrar) {
+    echo 'autenticado';
+} else {
+    echo 'fallo de autenticación';
+}
+
+?>
