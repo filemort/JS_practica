1faea1eca801a62adc1030315cf90d70b651daa8
filemort <filemort@gmail.com>
2025-03-08 20:21:28 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -3,9 +3,9 @@
 $contra_entrar="1234";
 $usu_entrar="Juan";
 
-$el_usuario=isset($_GET['usuario']) ? $_GET['usuario'] : $POST['usuario'];
+$el_usuario=isset($_GET['usuario']) ? $_GET['usuario'] : $_POST['usuario'];
 
-$la_contra=isset($_GET['contra']) ? $_GET['contra'] : $POST['contra'];
+$la_contra=isset($_GET['contra']) ? $_GET['contra'] : $_POST['contra'];
 
 if ($el_usuario == $usu_entrar && $la_contra == $contra_entrar) {
     echo 'autenticado';
