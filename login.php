ca2f2d7a910e6ee422e55cdb17089ae03c7eb61c
filemort <filemort@gmail.com>
2025-03-11 19:44:49 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ $el_usuario=isset($_GET['usuario']) ? $_GET['usuario'] : $_POST['usuario'];
 $la_contra=isset($_GET['contra']) ? $_GET['contra'] : $_POST['contra'];
 
 if ($el_usuario == $usu_entrar && $la_contra == $contra_entrar) {
-    echo 'autenticado';
+    echo 'validado';
 } else {
     echo 'fallo de autenticación';
 }
